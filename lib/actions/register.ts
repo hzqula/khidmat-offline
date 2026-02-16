7d83e9f4d8b11e84b1ed8a937d639e147f8b879e
hzqula <ilooqstrasi@gmail.com>
2026-02-16 10:06:10 +0700

    change data flow

--- lib/actions/register.ts
+++ lib/actions/register.ts
@@ -4,22 +4,32 @@ import { createClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
 import { prisma } from "@/lib/prisma";
 
-export async function register(prevState: unknown, formData: FormData) {
+export async function register(_: unknown, formData: FormData) {
   const supabase = await createClient();
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;
   const fullName = formData.get("fullName") as string;
 
-  console.log(prevState);
+  if (process.env.ALLOW_PUBLIC_REGISTRATION === "false") {
+    return { error: "Pendaftaran publik tidak diizinkan." };
+  }
+
   const { data, error } = await supabase.auth.signUp({ email, password });
 
   if (error) return { error: error.message };
 
   if (data.user) {
-    await prisma.profile.create({
-      data: {
+    await prisma.profile.upsert({
+      where: {
         id: data.user.id,
-        email: email,
+      },
+      update: {
+        email,
+        name: fullName,
+      },
+      create: {
+        id: data.user.id,
+        email,
         name: fullName,
       },
     });
